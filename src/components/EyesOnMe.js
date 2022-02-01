7a7acd860278564cca35675c7edc8660b445e60f
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-02-01 10:06:10 -0800

    commit tree c720e58e3f3bc9bda812310c42729dbc0366b32a

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import react from "react";
+
+function EyesOnMe() {
+    function focusCB() {
+        console.log('Good!')
+    }
+
+    function blurCB() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return <button onFocus={focusCB} onBlur={blurCB}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import react from "react";
+
+function Keypad() {
+    function changeCb() {
+        console.log('Entering password...')
+    }
+
+    return <input type='password' onChange={changeCb}/>
+}
+
+export default Keypad
